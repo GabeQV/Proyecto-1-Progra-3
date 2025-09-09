629c749d8c0771424fadf27f76651ea0c41e5288
Nashi <sebasgarro2006@gmail.com>
2025-09-08 20:50:49 -0600

    Momento autista, dw (Hice el panel de Prescripcion en Dashboard, les di vuelta y ya xd)

--- src/main/java/hospital/presentation/dashboard/DashboardView.java
+++ src/main/java/hospital/presentation/dashboard/DashboardView.java
@@ -3,17 +3,8 @@ package hospital.presentation.dashboard;
 import javax.swing.*;
 
 public class DashboardView {
-    private JPanel DashboardPanel;
-    private JButton buscarPacienteButton;
-    private JButton agregarMedicamentoButton;
-    private JPanel RecetaMedicaPanel;
-    private JPanel ControlPanel;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JTextField fechaDeRetiroTextField;
-    private JTextField mostrarPacienteAcaTextField;
 
-    public JPanel getDashboardPanel() {return DashboardPanel;}
+    private JPanel DashboardPanel;
+
+    public JPanel getPrescripcionPanel() {return DashboardPanel;}
 }
--- src/main/java/hospital/presentation/prescripcion/PrescripcionView.java
+++ src/main/java/hospital/presentation/prescripcion/PrescripcionView.java
@@ -3,8 +3,17 @@ package hospital.presentation.prescripcion;
 import javax.swing.*;
 
 public class PrescripcionView {
-
     private JPanel PrescripcionPanel;
+    private JButton buscarPacienteButton;
+    private JButton agregarMedicamentoButton;
+    private JPanel RecetaMedicaPanel;
+    private JPanel ControlPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JTextField fechaDeRetiroTextField;
+    private JTextField mostrarPacienteAcaTextField;
 
-    public JPanel getPrescripcionPanel() {return PrescripcionPanel;}
+    public JPanel getDashboardPanel() {return PrescripcionPanel;}
 }
