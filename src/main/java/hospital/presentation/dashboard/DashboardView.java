07b3d2c7e1cbcc66e06db48d5019882800d9b0a5
Nashi <sebasgarro2006@gmail.com>
2025-09-08 20:48:01 -0600

    Progreso Ventana Prescripcion, se metio la dependecia de GoodDatePicker

--- src/main/java/hospital/presentation/dashboard/DashboardView.java
+++ src/main/java/hospital/presentation/dashboard/DashboardView.java
@@ -4,6 +4,16 @@ import javax.swing.*;
 
 public class DashboardView {
     private JPanel DashboardPanel;
+    private JButton buscarPacienteButton;
+    private JButton agregarMedicamentoButton;
+    private JPanel RecetaMedicaPanel;
+    private JPanel ControlPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JTextField fechaDeRetiroTextField;
+    private JTextField mostrarPacienteAcaTextField;
 
     public JPanel getDashboardPanel() {return DashboardPanel;}
 }
