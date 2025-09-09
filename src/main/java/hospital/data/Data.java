329ad24e0f1b8f1ecafcebd8e7137d5fbc64c2b0
GabeQV <gabeqv16@gmail.com>
2025-09-08 20:32:45 -0600

    Implementacion de Data y sus listas

--- src/main/java/hospital/data/Data.java
+++ src/main/java/hospital/data/Data.java
@@ -1,4 +1,29 @@
 package hospital.data;
 
+import hospital.logic.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Data {
+    private List<Medico> medicos;
+    private List<Paciente> pacientes;
+    private List<Farmaceuta> farmaceutas;
+    private List<Medicamento> medicamentos;
+    private List<Receta> recetas;
+
+    public Data() {
+        medicos = new ArrayList<>();
+        pacientes = new ArrayList<>();
+        farmaceutas = new ArrayList<>();
+        medicamentos = new ArrayList<>();
+        recetas = new ArrayList<>();
+    }
+
+    public List<Medico> getMedicos() {return medicos;}
+    public List<Paciente> getPacientes() {return pacientes;}
+    public List<Farmaceuta> getFarmaceutas() { return farmaceutas; }
+    public List<Medicamento> getMedicamentos() { return medicamentos; }
+    public List<Receta> getRecetas() { return recetas; }
+
 }
