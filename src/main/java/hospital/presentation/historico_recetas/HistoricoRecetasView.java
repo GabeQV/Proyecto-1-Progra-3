c8004ae90573bb513a8b465cba83d11c8218ad1a
7joelb <joelbrvg@gmail.com>
2025-09-10 00:46:06 -0600

    View de HistoricoRecetas y Despacho

--- src/main/java/hospital/presentation/historico_recetas/HistoricoRecetasView.java
+++ src/main/java/hospital/presentation/historico_recetas/HistoricoRecetasView.java
@@ -1,4 +1,15 @@
 package hospital.presentation.historico_recetas;
 
+import javax.swing.*;
+
 public class HistoricoRecetasView {
+    private JTextField idPacienteTextField;
+    private JTextField idRecetaTextField;
+    private JButton buscarIdPacienteButton;
+    private JButton buscarIdRecetaButton;
+    private JTable table1;
+    private JPanel HistoricoRecetasPanel;
+    private JPanel Busqueda;
+
+    public JPanel getHistoricoRecetasView() {return  HistoricoRecetasPanel;}
 }
