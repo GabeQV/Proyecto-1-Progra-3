5d1c19b73d9711233cdad8c5b80be22687e3b88b
GabeQV <gabeqv16@gmail.com>
2025-09-10 14:18:46 -0600

    Creando Lista Farmaceutas

--- src/main/java/hospital/presentation/lista_farmaceutas/ListaFarmaceutasView.java
+++
@@ -1,9 +0,0 @@
-package hospital.presentation.lista_farmaceutas;
-
-import javax.swing.*;
-
-public class ListaFarmaceutasView {
-    private JPanel ListaFarmaceutasPanel;
-
-    public  JPanel getListaFarmaceutasPanel() {return ListaFarmaceutasPanel;}
-}
