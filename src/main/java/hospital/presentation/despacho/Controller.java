13b75fb02ee3486bf91172552634611afa6bca9f
GabeQV <gabeqv16@gmail.com>
2025-09-10 00:38:00 -0600

    Model y Controller de Despacho

--- src/main/java/hospital/presentation/despacho/Controller.java
+++ src/main/java/hospital/presentation/despacho/Controller.java
@@ -1,6 +1,6 @@
 package hospital.presentation.despacho;
 
-import hospital.logic.Medico;
+import hospital.logic.Receta;
 import hospital.logic.Service;
 import hospital.presentation.despacho.DespachoView;
 import hospital.presentation.despacho.Model;
@@ -12,29 +12,24 @@ public class Controller {
     public Controller(DespachoView view, Model model) {
         this.view = view;
         this.model = model;
-        view.setController(this);
-        view.setModel(model);
-        model.setList(Service.instance().findAllMedicos());
+        //view.setController(this);
+        //view.setModel(model);
+        model.setList(Service.instance().findAllRecetas());
     }
 
-    public void create(Medico e) throws  Exception{
-        Service.instance().createMedico(e);
-        if (e.getId() == null || e.getId().trim().isEmpty() || e.getId().contains(" ")) {
-            throw new Exception("El ID no puede estar vacío, repetido o contener espacios");
-        }
-
-        System.out.println("Guardando médico: " + e.getId() + " - " + e.getNombre() + " - " + e.getEspecialidad() + " - " + e.getClave());
-        model.setCurrent(new Medico());
-        model.setList(Service.instance().findAllMedicos());
+    public void create(Receta e) throws  Exception{
+        Service.instance().createReceta(e);
+        model.setCurrent(new Receta());
+        model.setList(Service.instance().findAllRecetas());
     }
 
     public void read(String id) throws Exception {
-        Medico e = new Medico();
+        Receta e = new Receta();
         e.setId(id);
         try {
-            model.setCurrent(Service.instance().readMedico(e));
+            model.setCurrent(Service.instance().readReceta(e));
         } catch (Exception ex) {
-            Medico b = new Medico();
+            Receta b = new Receta();
             b.setId(id);
             model.setCurrent(b);
             throw ex;
@@ -42,6 +37,6 @@ public class Controller {
     }
 
     public void clear() {
-        model.setCurrent(new Medico());
+        model.setCurrent(new Receta());
     }
 }
