4ec4b9586157a3b7b835e1273438b730e65a360e
GabeQV <gabeqv16@gmail.com>
2025-09-14 17:07:21 -0600

    Cambiando view de prescripcion

--- src/main/java/hospital/presentation/prescripcion/PrescripcionView.java
+++ src/main/java/hospital/presentation/prescripcion/PrescripcionView.java
@@ -33,64 +33,7 @@ public class PrescripcionView{
     public JPanel getPrescripcionPanel() {
         return PrescripcionPanel;
     }
-    public PrescripcionView() {
-        detallesButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFrame parent = (JFrame) SwingUtilities.getWindowAncestor(PrescripcionPanel);
-                ModificarDetalle dialog = new ModificarDetalle(parent, model);
-                dialog.setVisible(true);
-            }
-        });
-    }
-    public void setController(Controller controller) {
-        this.controller = controller;
-        buscarPacienteButton.addActionListener(e -> {
-            JFrame parent = (JFrame) SwingUtilities.getWindowAncestor(PrescripcionPanel);
-            BuscarPaciente dialog = new BuscarPaciente(parent, model);
-            dialog.setVisible(true);
-        });
 
-        agregarMedicamentoButton.addActionListener(e -> {
-            JFrame parent = (JFrame) SwingUtilities.getWindowAncestor(PrescripcionPanel);
-            AgregarMedicamento dialog = new AgregarMedicamento(parent, model);
-            dialog.setVisible(true);
-        });
 
-        descartarMedicamentoButton.addActionListener(e -> controller.borrarReceta(MedicamentosTable.getSelectedRow()));
-        guardarButton.addActionListener(e -> {
-            try {
-                controller.guardarRecetas(FechaDeRetiro.getText());
-            } catch (Exception ex) {
-                throw new RuntimeException(ex);
-            }
-        });
-        limpiarButton.addActionListener(e -> controller.clear());
-    }
 
-    public void setModel(Model model) {
-        this.model = model;
-    }
-
-    public void propertyChange(PropertyChangeEvent evt) {
-        switch (evt.getPropertyName()) {
-            case hospital.presentation.despacho.Model.LIST:
-                int[] cols = {
-                        TableModel.MEDICAMENTO,
-                        TableModel.PRESENTACION,
-                        TableModel.CANTIDAD,
-                        TableModel.INDICACIONES,
-                        TableModel.DURACION,
-                        TableModel.ESTADO
-                };
-                MedicamentosTable.setModel(new TableModel(cols, model.getList()));
-                break;
-            case Model.CURRENTPACIENTE:
-                // TODO: NO SE AAAHHHH
-                break;
-            case Model.CURRENTRECETA:
-                // TODO: NO SE AHHHHH
-                break;
-        }
-    }
 };
